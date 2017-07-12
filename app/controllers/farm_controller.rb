59c4d35397978923b80c14f6522eb5f9e232501a
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-12 16:05:16 -0500

    added session index/edited farms view & controller

--- app/controllers/farm_controller.rb
+++
@@ -1,4 +0,0 @@
-class FarmController < ApplicationController
-	def index
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'farm#index', as: 'farm'
+  root 'sessions#index'
 end
