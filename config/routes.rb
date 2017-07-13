1f7cf4b0d608a66656a82121fb232cd787974aaa
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-13 18:50:04 -0500

    added routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   root 'home#index'
+
+  resources :farms
+  resources :areas
+  resources :animals
+  resources :baby_animals
+  
 end
