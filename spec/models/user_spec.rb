4ff5fe032d9c7d7bb70bda94565f69c8aa6a9a5e
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-14 14:31:15 -0500

    added to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,34 @@ RSpec.describe User, type: :model do
 		@user = User.create(email: "test@email.com", password: "password")
 		@sapps = Farm.create(name: "Sapps Farm", user_id: @user.id)
 		@roberts = Farm.create(name: "Roberts Farm", user_id: @user.id)
+		@goat_pen = Area.create(name: "Goat Pen", farm_id: @sapps.id, area_type: "Pen", capacity: 20, quantity: 16)
+		@cocoa = Animal.create(
+			name: "Cocoa", 
+			age: "1 year", 
+			sex: "female", 
+			color: "white and tan", 
+			weight: 120, 
+			pregnant: true, 
+			date_bred: 20170407, 
+			estimated_due_date: 20171010, 
+			area_id: @goat_pen.id,
+			animal_type: "goat" 
+			)
+
+		@cocoa2 = Animal.create(
+			name: "Cocoa2", 
+			age: "1 year", 
+			sex: "female", 
+			color: "white and tan", 
+			weight: 120, 
+			pregnant: true, 
+			date_bred: 20170407, 
+			estimated_due_date: 20171010, 
+			area_id: @goat_pen.id,
+			animal_type: "goat" 
+			)
+		@user_animal1 = UserAnimal.create(@user.id, @cocoa.id)
+		@user_animal2 = UserAnimal.create(@user.id, @cocoa2.id)
 	end
 
 	it "is valid with an email and password" do
@@ -20,6 +48,11 @@ RSpec.describe User, type: :model do
 		
 		it "has many farms" do
 			expect(@user.farms.count).to eq(2)
+		end
+
+		it "has many animals through user animals" do
+			expect(@user.animals.first).to eq(@cocoa)
+			expect(@user.animals.second).to eq(@cocoa2)
 		end
 
 	end
