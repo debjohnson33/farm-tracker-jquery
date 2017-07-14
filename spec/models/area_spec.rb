6823acd93cb6176f9612bb444d448c97ad3f5d4d
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-14 16:32:34 -0500

    edited farm spec & added/edited area spec

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Farm, type: :model do
+RSpec.describe Area, type: :model do
 	before(:each) do
 		@user = User.create(email: "test@email.com", password: "password")
 		@sapps = Farm.create(name: "Sapps Farm", user_id: @user.id)
-		@roberts = Farm.create(name: "Roberts Farm", user_id: @user.id)
 		@goat_pen = Area.create(name: "Goat Pen", farm_id: @sapps.id, area_type: "Pen", capacity: 20, quantity: 16)
 		@pig_pen = Area.create(name: "Pig Pen", farm_id: @sapps.id, area_type: "Pen", capacity: 6, quantity: 4)
 		@cocoa = Animal.create(
@@ -32,29 +31,49 @@ RSpec.describe Farm, type: :model do
 			area_id: @goat_pen.id,
 			animal_type: "goat" 
 			)
+
+		@baby_cocoa = BabyAnimal.create(
+			name: "Baby Cocoa", 
+			age: "1 year", 
+			sex: "female", 
+			color: "white and tan", 
+			weight: 120,  
+			animal_id: @cocoa.id,
+			animal_type: "goat" 
+			)
+
+		@baby_cocoa2 = BabyAnimal.create(
+			name: "Baby Cocoa2", 
+			age: "1 year", 
+			sex: "female", 
+			color: "white and tan", 
+			weight: 120, 
+			animal_id: @cocoa.id,
+			animal_type: "goat" 
+			)		
 	end
 
-	it "is valid with a name and user_id" do
-		expect(@sapps).to be_valid
+	it "is valid with a name and animal_id" do
+		expect(@goat_pen).to be_valid
 	end
 
-	it "is not valid without a user_id" do
-		expect(Farm.new(name: "Sipsee Farm")).not_to be_valid
+	it "is not valid without a animal_id" do
+		expect(Area.new(name: "Goat Pen")).not_to be_valid
 	end		
 
 
 	context 'associations' do
 		
-		it "belongs to user" do
-			expect(@user.farms.count).to eq(2)
-		end
-
-		it "has many areas" do
+		it "belongs to farm" do
 			expect(@sapps.areas.count).to eq(2)
 		end
 
-		it "has many animals through areas" do
+		it "has many animals" do
 			expect(@sapps.areas.first.animals.count).to eq(2)
+		end
+
+		it "has many baby animals through animals" do
+			expect(@goat_pen.animals.first.baby_animals.count).to eq(2)
 			
 		end
 
