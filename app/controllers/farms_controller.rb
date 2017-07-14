bf09a85c46ec8d094403190b3adec1f2c9d5ffa2
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-13 20:36:09 -0500

    added new/create & started form for farms

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -6,4 +6,20 @@ class FarmsController < ApplicationController
 	def show
 		@farm = Farm.find(params[:id])
 	end
+
+	def new
+		@farm = Farm.new
+	end
+
+	def create
+		@farm = Farm.create(farm_params)
+		redirect_to farm_path(@farm)
+	end
+
+
+	private
+
+	def farm_params
+		params.require(:farm).permit(:name, :user_id)
+	end
 end
