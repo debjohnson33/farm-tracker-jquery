fda417354bc7b23332b71f27c4b0f72feadf8176
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-14 14:03:29 -0500

    started user model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+	before(:each) do
+		@user = User.create(email: "test@email.com", password: "password")
+		@sapps = Farm.create(name: "Sapps Farm", user_id: @user.id)
+		@roberts = Farm.create(name: "Roberts Farm", user_id: @user.id)
+	end
+
+	it "is valid with an email and password" do
+		expect(@user).to be_valid
+	end
+
+
+	context 'associations' do
+		
+		it "has many farms" do
+			expect(@user.farms.count).to eq(2)
+		end
+	end
+
+
+end
