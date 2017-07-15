888371edb0ed109d7a6a54dc732fa2919f26353e
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-14 19:42:28 -0500

    added create farm spec

---
+++ spec/features/create_farm_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'Add a Farm' do
+	scenario 'able to create a farm' do
+		#1 Go to home page and find a sign in link
+		#2 Click on link "Sign in" and get to sign in page
+		#3 Click on "Sign in" button and get to farms index 
+		#4 Click on "Add farm" and get to new farm form
+		#5 Click on "Create Farm" and get to farm view
+		#6 Farm view should have name (more after nested form is implemented)
+	end
+end
