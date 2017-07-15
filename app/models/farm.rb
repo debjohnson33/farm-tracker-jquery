7927a9d53cadbd64ff7cbd0c4eab76abb8033d80
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-15 12:44:49 -0500

    added validations to farm model

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,4 +2,9 @@ class Farm < ApplicationRecord
 	belongs_to :user
 	has_many :areas
 	has_many :animals, through: :areas
+
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	validates :user_id, presence: true
+	
 end
