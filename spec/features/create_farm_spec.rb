6448ec3dca7cce0d47b42f01777f5bd8a68417e9
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-14 19:59:05 -0500

    edited create farm spec

--- spec/features/create_farm_spec.rb
+++ spec/features/create_farm_spec.rb
@@ -3,9 +3,18 @@ require 'rails_helper'
 feature 'Add a Farm' do
 	scenario 'able to create a farm' do
 		#1 Go to home page and find a sign in link
+		visit '/'
 		#2 Click on link "Sign in" and get to sign in page
-		#3 Click on "Sign in" button and get to farms index 
+		click_link "Sign in"
+		fill_in "user_email", with: "test@email.com"
+		fill_in "password", with: "password"
+		#3 Click on "Sign in" button and get to farms index
+		click_link "Sign in"
 		#4 Click on "Add farm" and get to new farm form
+		click_link "Add farm"
+		
+		#5 Fill in the farm name field
+		fill_in "farm_name", with: "Phil's Farm"
 		#5 Click on "Create Farm" and get to farm view
 		#6 Farm view should have name (more after nested form is implemented)
 	end
