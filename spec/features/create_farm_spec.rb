38aee5e91668864dfa60517cbfae69b5b8197c5e
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-14 20:19:05 -0500

    edited create farm spec/added add farm link to farms index

--- spec/features/create_farm_spec.rb
+++ spec/features/create_farm_spec.rb
@@ -7,15 +7,17 @@ feature 'Add a Farm' do
 		#2 Click on link "Sign in" and get to sign in page
 		click_link "Sign in"
 		fill_in "user_email", with: "test@email.com"
-		fill_in "password", with: "password"
+		fill_in "user_password", with: "password"
 		#3 Click on "Sign in" button and get to farms index
-		click_link "Sign in"
+		click_button "Log in"
 		#4 Click on "Add farm" and get to new farm form
-		click_link "Add farm"
-		
+		click_link "Add Farm"
+
 		#5 Fill in the farm name field
 		fill_in "farm_name", with: "Phil's Farm"
 		#5 Click on "Create Farm" and get to farm view
+		click_button "Create Farm"
 		#6 Farm view should have name (more after nested form is implemented)
+		expect(page).to have_content("Phil's Farm")
 	end
 end
