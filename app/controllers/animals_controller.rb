7b39dd284cbcc01b8de1f4c6398b7f469208726a
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-18 12:25:58 -0500

    added create to animals controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -7,6 +7,35 @@ class AnimalsController < ApplicationController
 
 	def new
 		@farm = Farm.find([params[:farm_id]])
-		
+	end
+
+	def create
+		@animal = Animal.new(animal_params)
+		if @animal.save
+			redirect_to animal_path(@animal)
+		else
+			render new_farm_animal_path(@animal)
+		end
+	end
+
+
+	private
+
+	def animal_params
+		params.require(:animal).permit(
+			:id,
+			:name,
+			:area_id, 
+			:age,
+			:sex, 
+			:color,
+			:weight, 
+			:animal_type,
+			:pregnant,
+			:date_bred,
+			:estimated_due_date,
+			:area_id
+			]
+		)
 	end
 end
