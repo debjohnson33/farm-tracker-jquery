a8ea64d23374404971041b57a13f11f2f1c49b92
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-17 20:18:34 -0500

    edited attributes method

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -9,13 +9,7 @@ class Farm < ApplicationRecord
 
 	def areas_attributes=(areas_attributes)
 		areas_attributes.each do |i, area_attributes|
-			#area_attributes.each do |attribute|
-				#if attribute.empty?
-					# how do ignore the blank attributes?
-				#else 
-					self.areas.build(area_attributes)
-				#end
-			#end
+			self.areas.build(area_attributes) unless area_attributes.empty?
 		end
 	end
 	
