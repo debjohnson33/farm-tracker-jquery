2934342e881626d6349bbb20e44c6b4206bdd215
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-22 11:46:18 -0500

    edited animal/baby animal models to avoid empty baby animals in db

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -11,12 +11,16 @@ class Animal < ApplicationRecord
 	scope :goat, -> { where(animal_type: "goat") }
 	scope :large, -> { where('weight > ?', 100) }
 
+	before_save :erase_empty_baby_animals
+
 	def baby_animals_attributes=(baby_animals_attributes)
 		baby_animals_attributes.values.each do |baby_animal_attributes|
-			if !baby_animal_attributes.blank?
-				self.baby_animals.build(baby_animal_attributes)
-			end	
+				self.baby_animals.build(baby_animal_attributes)	
 		end
+	end
+
+	def erase_empty_baby_animals
+		self.baby_animals = self.baby_animals.select { |b| b.name != ''}
 	end
 
 end
--- app/models/baby_animal.rb
+++ app/models/baby_animal.rb
@@ -1,10 +1,4 @@
 class BabyAnimal < ApplicationRecord
 	belongs_to :animal
 
-	validates :name, presence: true
-	validates :age, presence: true
-	validates :sex, presence: true
-	validates :color, presence: true
-	validates :weight, presence: true
-
 end
