73402c9d04d30a513737128debc504a182371c77
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-27 19:22:40 -0500

    edited nested attributes method

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -9,12 +9,8 @@ class Farm < ApplicationRecord
 	accepts_nested_attributes_for :areas
 	
 	def areas_attributes=(areas_attributes)
-		areas_attributes.each do |area_attributes|
-			self.area = Area.where(name: area_attributes[:name]).first or create do |a|
-				a.area_type = areas_attributes[:area_type]
-				a.capacity = areas_attributes[:capacity]
-	    		a.quantity = areas_attributes[:quantity]
-	    	end
+		areas_attributes.each do |i, area_attributes|
+			self.areas.build(area_attributes)
 		end
 	end
 
