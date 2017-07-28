e1df3c301cbada2b688b4bb5fd1d51676e3d1024
debjohnson33 <debjohnson_11@yahoo.com>
2017-07-28 15:04:57 -0500

    added error messages to area new page/edited area controller

--- app/models/area.rb
+++ app/models/area.rb
@@ -3,6 +3,9 @@ class Area < ApplicationRecord
 	has_many :animals
 
 	validates :name, presence: true
+	validates :area_type, presence: true
+	validates :capacity, presence: true
+	validates :quantity, presence: true
 
 	def availability
 		self.capacity - self.quantity
