f594b479054a56bf56c0a9564e148b6b5740115e
debjohnson33 <debjohnson_11@yahoo.com>
2017-10-23 18:11:45 -0500

    took out debugger line/console logs

--- app/assets/javascripts/animals.js
+++ app/assets/javascripts/animals.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 	$('a.load_animals').on('click', function(e) {
-		
-		console.log('Clicked on Animals Link!')
+
 		$.ajax({
 			method: 'GET',
 			url: this.href
 		}).success(function(data) {
-			console.log(data)
-			//debugger
+	
 			var $ol = $("#animals_list ol")
 			$ol.html("")
 
