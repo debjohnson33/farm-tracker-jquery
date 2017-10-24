750beba0b0941ff52c406609217528e24821a297
debjohnson33 <debjohnson_11@yahoo.com>
2017-10-23 21:08:54 -0500

    edited area show & js to implement next button

--- app/assets/javascripts/areas.js
+++ app/assets/javascripts/areas.js
@@ -6,7 +6,14 @@ const bindClickHandlers = () => {
 	$('.js-next').on('click', function() {
 		
 		console.log('Clicked on Next Area')
-		var nextId = parseInt($(".js-next").attr(data-id)) + 1;
-		debugger
+		var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+		$.get("/areas/" + nextId + ".json", function(data) {
+			//debugger
+			$(".areaName").text(data["name"])
+			$(".areaType").text(data["type"])
+			$(".areaCapacity").text(data["capacity"])
+			$(".areaQuantity").text(data["quantity"])
+			$(".areaAvailability").text(data["availability"])
+		})
 	})
 }
