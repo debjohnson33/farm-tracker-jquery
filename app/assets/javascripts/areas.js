399d758fea5b825ffb81abb02a6e72e1233241ba
debjohnson33 <debjohnson_11@yahoo.com>
2017-10-25 13:54:31 -0500

    reverted area js to changing text data of area show page

--- app/assets/javascripts/areas.js
+++ app/assets/javascripts/areas.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
 
 const bindClickHandlers = () => {
 	$('.js-next').on('click', function() {
-		console.log('Clicked Next')
+		
 		var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-		$.get("/areas/" + nextId + ".json", function(area) {
-			$(".body-container").html('')
-			let newArea = new Area(area)
-			let areaHtml = newArea.formatShow()
-			$(".body-container").append(areaHtml)
+		$.get("/areas/" + nextId + ".json", function(data) {
+			//debugger
+			$(".areaName").text(data["name"])
+			$(".areaType").text(data["type"])
+			$(".areaCapacity").text(data["capacity"])
+			$(".areaQuantity").text(data["quantity"])
+			$(".areaAvailability").text(data["availability"])
 		})
 	})
 }
@@ -20,17 +22,8 @@ function Area(area) {
 	this.id = area.id
 	this.name = area.name
 	this.area_type = area.area_type
-	this.capacity = area.capacity
+	this.area_capacity = area.area_capacity
 	this.quantity = area.quantity
 	this.availability = area.availability
 }
 // Prototype Method
-Area.prototype.formatShow = function() {
-	let areaHtml = `
-		<h1>Area</h1>
-		<h2>Name: ${this.name}</h2>
-		<p>Capacity: ${this.capacity}</p>
-		<p>Quantity: ${this.quantity}</p>
-	`
-	return areaHtml
-}
