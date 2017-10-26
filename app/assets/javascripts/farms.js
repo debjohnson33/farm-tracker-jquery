fd7540295eb5c161a9661548a775b08157b51fe2
debjohnson33 <debjohnson_11@yahoo.com>
2017-10-26 17:17:46 -0500

    added areas to json farm show view

--- app/assets/javascripts/farms.js
+++ app/assets/javascripts/farms.js
@@ -25,7 +25,15 @@ const bindClicks = () => {
 			$(".body-container").html('')
 				let newFarm = new Farm(farm)
 				let farmHtml = newFarm.formatShow()
-				$(".body-container").append(farmHtml)			
+				$(".body-container").append(farmHtml)
+
+				farm.areas.forEach(function(area) {
+					let newArea = new Area(area)
+					let areaHtml = newArea.formatShow()
+					$(".body-container").append(areaHtml)
+				})
+				
+				//debugger			
 		})
 	})
 }
@@ -34,12 +42,13 @@ const bindClicks = () => {
 function Farm(farm) {
 	this.id = farm.id
 	this.name = farm.name
+	this.areas = farm.areas
 }
 // Prototype Method
 Farm.prototype.formatIndex = function() {
 	let farmHtml = `
 		<h1>Farms</h1>
-		<a href="/farms/${this.id}" data-id=${this.id} class="show_link"><h2>${this.name}</h2></a>
+		<a href="/farms/${this.id}" data-id=${this.id} class="show_link">${this.name}</a><br><br>
 		<a href="/farms/new" class="add_farm_link">Add Farm</a>
 	`
 	return farmHtml
@@ -47,11 +56,21 @@ Farm.prototype.formatIndex = function() {
 
 Farm.prototype.formatShow = function() {
 	let farmHtml = `
-		<h1>Farm</h1>
-		<h2>Name: ${this.name}</h2>
-		<h2>Id: ${this.id}</h2>
+		<h2>Farm</h2>
+		<p>Name: ${this.name}</p>
+		<p>Id: ${this.id}</p>
 	`
 	return farmHtml
 }
 
+function Area(area) {
+	this.name = area.name
+}
+
+Area.prototype.formatShow = function() {
+	let areaHtml = `
+		<p>${this.name}</p>
+	`
+	return areaHtml
+}
 
