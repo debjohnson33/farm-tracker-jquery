b10e1a1966a827cdc4959d8359b7e94ea9662a67
debjohnson33 <debjohnson_11@yahoo.com>
2017-10-31 19:25:34 -0500

    added jquery click handler to home link

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	onClickHandlers();
+})
+
+const onClickHandlers = () => {
+	$(document).on('click', 'a.navbar-brand', function(e) {
+		e.preventDefault();
+		console.log("Did It!")
+		$(".body-container").html('')
+		$(".body-container").append(formatHome());
+	})
+}
+
+function formatHome() {
+	let homeHtml = `
+		<br><br>
+		<div class=notice id=notice><div>
+		<div class=alert id=alert><div>
+		<h1>Welcome to the Farm Tracker!</h1>
+		<h2>Click on one of the links</h2>
+		<h2>at the top of the page to sign up or sign in</h2>
+	`
+	return homeHtml
+}
